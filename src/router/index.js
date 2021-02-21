875fe4283da091ff1e331b72ec659ae0eb0e6419
Victor Peter <ukokjnr@gmail.com>
2021-02-21 11:09:12 +0000

    add route to profile

--- src/router/index.js
+++ src/router/index.js
@@ -40,9 +40,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  // history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
-  mode: 'history'
 });
 
 export default router;
